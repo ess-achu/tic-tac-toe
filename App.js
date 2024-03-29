ed59ebcc34d2d594d3d8fb8b6af710f0231550c9
Swaraj <swaraj.achu.50@gmail.com>
2024-03-29 22:36:45 +0530

    Added win logics

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  Touchable,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { useEffect, useMemo, useState } from "react";
 
 var mainGameArray = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
-var currentPlayer = "O";
+var currentPlayer = "X";
 var isOver = false;
 
 export default function App() {
@@ -18,6 +24,9 @@ export default function App() {
       });
     }
     setGameArray(newBlocks);
+    currentPlayer = "X";
+    isOver = false;
+    
   };
 
   //Winner check logic
@@ -85,11 +94,12 @@ export default function App() {
     if (checkWinColumn() || checkWinDiagonal() || checkWinRow()) {
       return true;
     }
+    return false;
   };
 
   //Game logic on selecting a column
   const gameLogic = (cell) => {
-    if (!cell.isSelected) {
+    if (!cell.isSelected && !isOver) {
       const newGameArray = gameArray.map((gameCell) => {
         if (gameCell.pos === cell.pos && !gameCell.isSelected) {
           gameCell.player = currentPlayer;
@@ -98,6 +108,14 @@ export default function App() {
         return gameCell;
       });
       setGameArray(newGameArray);
+      isOver = checkWin();
+      if (!isOver) {
+        if (currentPlayer === "X") {
+          currentPlayer = "O";
+        } else {
+          currentPlayer = "X";
+        }
+      }
     }
   };
 
@@ -113,16 +131,55 @@ export default function App() {
         {gameArray.map((cell, i) => {
           return (
             <TouchableOpacity
-              style={!cell.isSelected ? styles.notSelected : cell.player === "X" ? styles.xSelected : styles.oSelected }
+              style={
+                !cell.isSelected
+                  ? styles.notSelected
+                  : cell.player === "X"
+                  ? styles.xSelected
+                  : styles.oSelected
+              }
               key={i}
               onPress={() => gameLogic(cell)}
             >
-              <Text>{cell.player}</Text>
+              <Text style={cell.player === "X" ? styles.xText : styles.oText}>
+                {cell.player}
+              </Text>
             </TouchableOpacity>
           );
         })}
       </View>
-      <Text style={styles.playerText}>{currentPlayer}'s turn</Text>
+      {isOver ? (
+        <Text
+          style={
+            currentPlayer === "X" ? styles.xPlayerText : styles.oPlayerText
+          }
+        >
+          {currentPlayer} won
+        </Text>
+      ) : (
+        <Text
+          style={
+            currentPlayer === "X" ? styles.xPlayerText : styles.oPlayerText
+          }
+        >
+          {currentPlayer}'s turn
+        </Text>
+      )}
+      <TouchableOpacity
+        style={{
+          width: 300,
+          height: 30,
+          borderWidth: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: 2,
+        }}
+        onPress={() => start()}
+      >
+        <Text style={{ fontSize: 15, fontWeight: "600", fontFamily: "Roboto" }}>
+          Play Again
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -132,13 +189,13 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     paddingTop: 100,
-    backgroundColor: "#153E7E",
+    backgroundColor: "#0000",
   },
-  texctHeading: { fontSize: 40, fontWeight: "500", color: "#E2F4C5" },
+  texctHeading: { fontSize: 40, fontWeight: "500", color: "black" },
   container: {
     width: 350,
     height: 350,
-    backgroundColor: "#00003F",
+    backgroundColor: "#0000",
     marginTop: 50,
     flexDirection: "row",
     flexWrap: "wrap",
@@ -152,26 +209,45 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   colors: {
-    rain: "#F3F3F3",
-    gameRed: "#DF2E38",
+    rain: "#60C4FF",
+    gameRed: "#D8A99E",
     gameGreen: "#5D9C59",
   },
   xSelected: {
     width: 100,
     height: 100,
-    backgroundColor: "#DF2E38",
+    backgroundColor: "#60C4FF",
     margin: 8,
+    opacity: 0.4,
+    justifyContent: "center",
   },
   notSelected: {
     width: 100,
     height: 100,
     backgroundColor: "#00003F",
     margin: 8,
+    opacity: 0.2,
   },
   oSelected: {
     width: 100,
     height: 100,
-    backgroundColor: "#5D9C59",
+    backgroundColor: "#D8A99E",
     margin: 8,
+    opacity: 0.4,
+    justifyContent: "center",
+  },
+  oText: { color: "red", fontSize: 40, textAlign: "center" },
+  xText: { color: "blue", fontSize: 40, textAlign: "center" },
+  oPlayerText: {
+    color: "#D8A99E",
+    fontSize: 40,
+    textAlign: "center",
+    fontFamily: "sans-serif",
+  },
+  xPlayerText: {
+    color: "#60C4FF",
+    fontSize: 40,
+    textAlign: "center",
+    fontFamily: "Roboto",
   },
 });
